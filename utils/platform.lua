2c9b1619ba8d89015c51d0feee49b29abddbb23e
Adam K <arkelly2@medicine.wisc.edu>
2024-02-13 21:39:03 -0600

    modify platform checker

--- utils/platform.lua
+++ utils/platform.lua
@@ -1,21 +1,10 @@
 local wezterm = require "wezterm"
 
-local function is_found(str, pattern)
-  return string.find(str, pattern) ~= nil
-end
+local M = {}
 
-local function platform()
-  local is_win = is_found(wezterm.target_triple, "windows")
-  local is_linux = is_found(wezterm.target_triple, "linux")
-  local is_mac = is_found(wezterm.target_triple, "apple")
-  local os = is_win and "windows" or is_linux and "linux" or is_mac and "mac" or "unknown"
-  return {
-    os = os,
-    is_win = is_win,
-    is_linux = is_linux,
-    is_mac = is_mac,
-  }
-end
-
-return platform
+return {
+  is_win = string.find(wezterm.target_triple, "windows") ~= nil,
+  is_linux = string.find(wezterm.target_triple, "linux") ~= nil,
+  is_mac = string.find(wezterm.target_triple, "apple") ~= nil,
+}
 
